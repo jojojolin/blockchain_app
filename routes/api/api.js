e47b44daaa92345383cdaf3b3f83d0aa803321b6
zhenggzh3 <zhenggzh3@163.com>
2018-06-17 10:48:32 +0800

    --2 commit

--- routes/api/api.js
+++ routes/api/api.js
@@ -4,13 +4,13 @@ var request = require("request");
 
 
 router.post('/wx/token', function (req, res, next) {
-    let appid='wx458de2a971d24b5e';
-    let secret='a78f7cfd97a33f83023e24941d95e34c';
+    let appid='wx18cc75dbf1ca30aa';
+    let secret='bce9584cf3b1c35a1833fd158e9e2e0d';
     let grant_type='authorization_code';
     let js_code = req.body.code;
-
+	
     let param = '?appid='+appid+"&secret="+secret+"&js_code"+js_code+"&grant_type="+grant_type;
-
+	
     request({
         url:'GET https://api.weixin.qq.com/sns/jscode2session'+param,
         method:'GET',
@@ -19,7 +19,7 @@ router.post('/wx/token', function (req, res, next) {
         if(!error && response.statusCode===200){
             console.log("response: "+response);
             console.log("body: "+body);
-            res.json(body);
+            res.json({data: body});
         }
     });
 });
