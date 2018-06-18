99ee1114c20427075c40ba4e547a58af14549c8a
zhenggzh3 <zhenggzh3@163.com>
2018-06-18 14:48:26 +0800

    add token

--- routes/api/api.js
+++ routes/api/api.js
@@ -13,10 +13,14 @@ router.post('/wx/token', function (req, res, next) {
     let grant_type='authorization_code';
     let js_code = req.body.code;
     let param = '?appid='+appid+"&secret="+secret+"&js_code"+js_code+"&grant_type="+grant_type;
-
+	console.log("/wx/token");
+	res.json({status :0});
+	
+	
+	/*
     request({
-        ///url:'GET https://api.weixin.qq.com/sns/jscode2session'+param,
-		url:'GET https://www.baidu.com'
+        url:'GET https://api.weixin.qq.com/sns/jscode2session'+param,
+		//url:'GET https://www.baidu.com'
         method:'GET',
         headers:{'Content-Type':'text/json' }
     }, function(error, response, body){
@@ -26,7 +30,7 @@ router.post('/wx/token', function (req, res, next) {
 			//console.log("openid: "+wechat_openid);
 			console.log(body);
 			
-			/*
+			
             WxUser.findOne({where: {openid: wechat_openid}})
                 .then(function (result) {
                     if(result==null){// 用户不存在
@@ -58,9 +62,9 @@ router.post('/wx/token', function (req, res, next) {
                 .catch(function (err) {
                     console.log("error");
                 });
-			*/
         }
     });
+	*/
 });
 
 
