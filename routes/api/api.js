946a412bee0d2f4531c4a1b7f89e4f038fb8c570
zhenggzh3 <zhenggzh3@163.com>
2018-06-18 11:53:49 +0800

    add tokrn

--- routes/api/api.js
+++ routes/api/api.js
@@ -1,29 +1,84 @@
 var express = require('express');
 var router = express.Router();
 var request = require("request");
+var WxUser = require("../../models/WxUser");
+var sequelize = require('sequelize');
+var jsonwebtoken = require('jsonwebtoken');
 
 
 router.post('/wx/token', function (req, res, next) {
-    let appid='wx18cc75dbf1ca30aa';
-    let secret='bce9584cf3b1c35a1833fd158e9e2e0d';
+    let appid='wx458de2a971d24b5e';
+    let secret='a78f7cfd97a33f83023e24941d95e34c';
     let grant_type='authorization_code';
     let js_code = req.body.code;
-	
     let param = '?appid='+appid+"&secret="+secret+"&js_code"+js_code+"&grant_type="+grant_type;
-	
+
     request({
         url:'GET https://api.weixin.qq.com/sns/jscode2session'+param,
         method:'GET',
         headers:{'Content-Type':'text/json' }
     }, function(error, response, body){
         if(!error && response.statusCode===200){
-            console.log("response: "+response);
-            console.log("body: "+body);
-            res.json({data: body});
+            body = JSON.parse(body);
+            let wechat_openid = body.openid;
+
+            WxUser.findOne({where: {openid: wechat_openid}})
+                .then(function (result) {
+                    if(result==null){// 用户不存在
+                        let user = {
+                            openid: wechat_openid,
+                            score: 0,
+                        };
+                        WxUser.create(user)
+                            .then(function (result) {
+                                //生成token
+                                let userid = result.id;
+                                let token = passport.endcode({
+                                    userid: userid,
+                                    openid: wechat_openid,
+                                });
+                                res.json({ token:token });
+                            });
+                    }
+                    else{// 用户存在
+                        // 生成token
+                        let userid = result.id;
+                        let token = passport.endcode({
+                            userid: userid,
+                            openid: wechat_openid,
+                        });
+                        res.json({ token:token });
+                    }
+                })
+                .catch(function (err) {
+                    console.log("error");
+                });
         }
     });
 });
 
 
+router.post('/wx/reg', function (req, res, next){
+    res.json({msg: 'this is /wx/reg'});
+});
+
+
+router.post('/wx/bet', function (req, res, next){
+    let token = req.headers['token'];
+
+    jsonwebtoken.verify(token, "this is a secret", function (err, decoded) {
+        if (!err){
+            console.log("error: jsonwebtoken.verify");
+            res.json({status :0}); //token无效
+        }
+        else{
+            console.log(decoded);
+        }
+    })
+});
+
+
 module.exports = router;
+
+
 
---
+++ routes/api/passport.js
@@ -0,0 +1,39 @@
+const jsonwebtoken = require('jsonwebtoken');
+
+
+function endcode(payload){
+    return jsonwebtoken.sign({
+        data: payload,
+        exp: (60 * 60) + Math.floor(Date.now() / 1000),  // 设置 token 过期时间
+    }, "this is a secret");
+}
+
+
+function decode(token){
+    return jsonwebtoken.decode(token.split(' ')[1], "this is a secret")
+}
+
+
+function verify(token){
+    return jsonwebtoken.verify(token.split(' ')[1], "this is a secret")
+}
+
+
+function verifyid(token, idname, id){
+    let payload=this.verify(token);
+    if(payload.data[idname]==id){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+module.exports = {
+    endcode: endcode,
+    decode: decode,
+    verify: verify,
+    verifyid: verifyid
+};
+
